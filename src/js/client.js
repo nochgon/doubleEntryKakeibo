d3e85d83b10ddf9d32e783ffd42d555e5b8f8ed9
nochgon <nochgon@gmail.com>
2021-07-27 20:14:47 +0900

    テーブルの位置変更
    テーブルを少し真中へ移動した。

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {DualTable} from "./mergeTable";
 import {DualEntry} from "./dualEntry";
 import {ObjectRepository} from "./repository/objectRepository"
 
-/*
-const listLeft = ["aaa", "bbb"];
-const listRight = ["ccc", "ddd", "eee"];
-*/
 
 const debt = {"食費": 10000, "書籍": 7000, "小物": 2000, "交通費": 1000};
 const credit = {"現金": 10000, "Suica": 10000}
@@ -21,7 +16,7 @@ let repository = new ObjectRepository(debt, credit);
 class Layout extends React.Component {
     render() {
         return (
-            <div>
+            <div style={{position: 'relative', left: '50px'}}>
                 <br></br>
                 <DualEntry debt={repository._debt} credit={repository._credit} styleTable={styleTable} />
             </div>
